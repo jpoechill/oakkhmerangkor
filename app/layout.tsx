e001e5c07b28c6a239de66d954891f0b80b527c1
jpoechill <poamrong@rith.com>
2024-06-03 11:14:26 -0700

    add meta cover img

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Oakland Khmer Angkor Dance Troupe",
   description: "Discover the Oakland Khmer Angkor Dance Troupe, a vibrant Cambodian youth traditional dance group in Oakland, CA. Celebrating over 25 years of cultural heritage and artistic excellence. Join us in preserving and promoting the beauty of Khmer dance.",
+  openGraph: {
+    images: '/metaimg.jpg',
+  },
 };
 
 export default function RootLayout({
