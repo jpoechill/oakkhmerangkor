0c090762eb1638e2a58dd646f53725558ac210bf
jpoechill <poamrong@rith.com>
2024-06-13 22:41:03 -0700

    unlink dances detail

--- app/dances/page.tsx
+++ app/dances/page.tsx
@@ -20,18 +20,18 @@ export default function Home() {
               </div>
             </div><br /><br />
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-              <Link href="/dances/apsara">
-                <div className="group hover:cursor-pointer relative h-0 w-[100%] pb-[100%] overflow-hidden rounded-2xl">
-                  <div className="absolute h-full font-bold w-full z-10 tracking-widest bg-black opacity-25 group-hover:opacity-0 transition-all duration-500 flex text-xl justify-center items-center">
-                  </div>
-                  <div className="absolute h-full font-bold w-full z-10 tracking-widest text-white flex text-xl justify-center items-center">
-                    APSARA
-                  </div>
-                  <div className="">
-                    <Image src="/thumbs/apsara.png" alt="thumb" className="transition-all duration-500 ease-in-out group-hover:scale-[1.10]" fill></Image>
-                  </div>
+              {/* <Link href="/dances/apsara"> */}
+              <div className="group hover:cursor-pointer relative h-0 w-[100%] pb-[100%] overflow-hidden rounded-2xl">
+                <div className="absolute h-full font-bold w-full z-10 tracking-widest bg-black opacity-25 group-hover:opacity-0 transition-all duration-500 flex text-xl justify-center items-center">
                 </div>
-              </Link>
+                <div className="absolute h-full font-bold w-full z-10 tracking-widest text-white flex text-xl justify-center items-center">
+                  APSARA
+                </div>
+                <div className="">
+                  <Image src="/thumbs/apsara.png" alt="thumb" className="transition-all duration-500 ease-in-out group-hover:scale-[1.10]" fill></Image>
+                </div>
+              </div>
+              {/* </Link> */}
               <div className="group hover:cursor-pointer relative h-0 w-[100%] pb-[100%] overflow-hidden rounded-2xl">
                 <div className="absolute h-full font-bold w-full z-10 tracking-widest bg-black opacity-25 group-hover:opacity-0 transition-all duration-500 flex text-xl justify-center items-center">
                 </div>
