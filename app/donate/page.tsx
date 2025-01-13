559137d448eba6f147e289b5182ef676f93cf98a
jpoechill <poamrong@rith.com>
2025-01-13 15:16:18 -0800

    add email js

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -17,27 +17,41 @@ export default function Home() {
               </div>
               <div className="text-3xl font-bold tracking-wide">
                 {/* Support Our Mission */}
-                Preserving Culture, Inspiring Generations
+                Support Our Mission
               </div>
             </div>
             <br />
+            The Oakland Khmer Angkor Dance Troupe is dedicated to preserving the rich heritage of traditional Cambodian dance and passing it on to future generations. Our troupe serves as a cultural bridge, sharing the beauty of classical and folk Khmer dances with the community and beyond.
+            <br />
+            <br />
 
-            <div>
-              The Oakland Khmer Angkor Dance Troupe is dedicated to preserving the rich heritage of traditional Cambodian dance and passing it on to future generations. Our troupe serves as a cultural bridge, sharing the beauty of classical and folk Khmer dances with the community and beyond.
-              <br /><br />
-              Your generosity enables us to: <br />
-              <ul className="list-disc pl-8 pt-2">
-                <li className="pt-2">Provide free or affordable dance classes to youth and adults in the community.</li>
-                <li className="pt-2">Sustain traditional costumes, instruments, and teaching materials, keeping our performances authentic.</li>
-                <li className="pt-2">Host cultural showcases and community events, celebrating Cambodian heritage and engaging the public.</li>
-                <li className="pt-2">Support our talented dancers and instructors, ensuring the continuation of this art form.</li>
-              </ul>
-              <br />
-              Every donation, big or small, makes a difference. By giving, you become a part of a legacy that transcends generations, connecting us to our roots and sharing Khmer culture with the world.
-              {/* Support our cause by making a donation through our secure Stripe payment system. Your generous contribution helps us continue our important work and make a difference in the community. */}
-              <br /><br />
-              {/* Thank you for your support and for helping us create a positive impact! */}
+            <div className="font-light text-md relative grid grid-cols-1 lg:grid-cols-8 w-full gap-6 lg:gap-10">
+
+              <div className="col-span-8 lg:col-span-5">
+                {/* The Oakland Khmer Angkor Dance Troupe is dedicated to preserving the rich heritage of traditional Cambodian dance and passing it on to future generations. Our troupe serves as a cultural bridge, sharing the beauty of classical and folk Khmer dances with the community and beyond. */}
+                {/* <br /><br /> */}
+                Your generosity enables us to: <br />
+                <ul className="list-disc pl-8 pt-2">
+                  <li className="pt-2">Provide free or affordable dance classes to youth and adults in the community, fostering inclusivity and skill-building.</li>
+                  <li className="pt-2">Sustain traditional costumes, instruments, and teaching materials, keeping our performances authentic.</li>
+                  <li className="pt-2">Host cultural showcases and community events, celebrating Cambodian heritage and engaging the public.</li>
+                  <li className="pt-2">Support our talented dancers and instructors, ensuring the continuation of this art form.</li>
+                  <li className="pt-2">Partner with local and international cultural organizations to promote the exchange of knowledge and performances.</li>
+                </ul>
+                {/* <br /> */}
+                {/* Every donation, big or small, makes a difference. By giving, you become a part of a legacy that transcends generations, connecting us to our roots and sharing Khmer culture with the world. */}
+
+                {/* Thank you for your support and for helping us create a positive impact! */}
+              </div>
+              <div className="col-span-8 lg:col-span-3 mb-0 lg:mb-4 group bg-cover bg-black relative h-0 w-full pb-[100%] hover:cursor-pointer rounded-2xl overflow-hidden">
+                <Image src="/getintouch_02.png" alt="thumb" className="bg-contain transition-all duration-500 h-full w-full ease-in-out" fill></Image>
+              </div>
             </div>
+            <br />
+            Every donation, big or small, makes a difference. By giving, you become a part of a legacy that transcends generations, connecting us to our roots and sharing Khmer culture with the world.
+            {/* Support our cause by making a donation through our secure Stripe payment system. Your generous contribution helps us continue our important work and make a difference in the community. */}
+            <br />
+            <br />
             <br />
             {/* <br /><br /> */}
             <div className="grid grid-cols-2 gap-4">
